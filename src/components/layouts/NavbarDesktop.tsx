07d2ef050b55cd22025331f3a7bf780e826e1465
souleymanesy7 <souleymanesyservices@gmail.com>
2025-04-15 00:15:21 +0000

    feat: add the desktop navbar component

---
+++ src/components/layouts/NavbarDesktop.tsx
@@ -0,0 +1,70 @@
+import * as React from "react";
+import Link from "next/link";
+
+import Container from "../common/Container";
+import VisuallyHidden from "../common/VisuallyHidden";
+
+import {
+  InstagramIcon,
+  LogoIcon,
+  RSSIcon,
+  TwitterIcon,
+  YoutubeIcon,
+} from "@/icons/Icons.component";
+import List from "../common/List";
+
+const navbarList = ["Magazine", "Podcast", "Author"];
+const socialIcons = [
+  { id: 0, Icon: InstagramIcon, href: "https://instagram.com" },
+  { id: 1, Icon: TwitterIcon, href: "https://x.com" },
+  { id: 2, Icon: YoutubeIcon, href: "https://youtube.com" },
+  { id: 3, Icon: RSSIcon, href: "https://rss.com" },
+];
+
+const NavbarDesktop = () => {
+  return (
+    <Container
+      as={"div"}
+      className="lg:flex lg:justify-between lg:items-center hidden"
+    >
+      <Link href={"/"}>
+        <LogoIcon />
+        <VisuallyHidden>Fyrre magazine logo</VisuallyHidden>
+      </Link>
+
+      <Container as="ul" className="flex items-center gap-4">
+        <List
+          items={navbarList}
+          className="flex items-center gap-4"
+          renderItem={(item) => {
+            return (
+              <li key={item}>
+                <Link href={`/${item}`}>{item}</Link>
+              </li>
+            );
+          }}
+        />
+
+        <span className="inline-block w-4 h-[1px] bg-primary-clr"></span>
+
+        <List
+          items={socialIcons}
+          className="flex items-end gap-2"
+          renderItem={(item) => {
+            const Icon = item.Icon;
+
+            return (
+              <li key={item.id}>
+                <Link href={item.href}>
+                  <Icon width={20} height={20} />
+                </Link>
+              </li>
+            );
+          }}
+        />
+      </Container>
+    </Container>
+  );
+};
+
+export default NavbarDesktop;
