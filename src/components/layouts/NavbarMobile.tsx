6f2cfe6bed52292fa8aa2477142f9899274d2d15
souleymanesy7 <souleymanesyservices@gmail.com>
2025-04-21 20:39:21 +0000

    feat: add the navbar for mobile version

---
+++ src/components/layouts/NavbarMobile.tsx
@@ -0,0 +1,74 @@
+import * as React from "react";
+import Link from "next/link";
+
+import clsx from "clsx";
+import FocusLock from "react-focus-lock";
+
+import List from "../common/List";
+import Container from "../common/Container";
+
+import { ArrowRightIcon } from "@/icons/Icons.component";
+import useOnDisabledBodyScroll from "@/hooks/useOnDisabledBodyScroll";
+
+type NavbarMobilePropsType = {
+  isOpen: boolean;
+};
+
+const navbarMobileList = [
+  {
+    id: 0,
+    href: "/Magazine",
+    title: "Magazine",
+  },
+  {
+    id: 1,
+    href: "/Podcast",
+    title: "Podcast",
+  },
+  {
+    id: 2,
+    href: "/Author",
+    title: "Author",
+  },
+];
+
+const NavbarMobile: React.FC<NavbarMobilePropsType> = ({ isOpen }) => {
+  useOnDisabledBodyScroll(isOpen);
+
+  return (
+    <React.Fragment>
+      <FocusLock returnFocus={false}>
+        <Container
+          as={"div"}
+          className={clsx(
+            "container | lg:hidden absolute top-0 left-0 z-10 -translate-y-[100%] bg-default-clr w-full pb-2 transition-transform duration-500 ease-in-out",
+            isOpen && "translate-y-[77px]"
+          )}
+        >
+          <List
+            items={navbarMobileList}
+            renderItem={(item) => {
+              return (
+                <li
+                  key={item.id}
+                  id="mobile-navigation-link"
+                  className="border-b border-primary-clr"
+                >
+                  <Link
+                    href={item.href}
+                    className="flex items-center justify-between py-3"
+                  >
+                    <span className="uppercase font-medium">{item.title}</span>
+                    <ArrowRightIcon />
+                  </Link>
+                </li>
+              );
+            }}
+          />
+        </Container>
+      </FocusLock>
+    </React.Fragment>
+  );
+};
+
+export default NavbarMobile;
