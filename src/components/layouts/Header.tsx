77c887ca0d3354d6573c13852937fca77e765f05
souleymanesy7 <souleymanesyservices@gmail.com>
2025-04-21 20:41:34 +0000

    feat: add the header component

---
+++ src/components/layouts/Header.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Navbar from "./Navbar";
+
+const Header = () => {
+  return (
+    <header className="container relative z-50 p-6">
+      <Navbar />
+    </header>
+  );
+};
+
+export default Header;
