3eb5a876ccf70631a22c397aa0e059c27099c94f
souleymanesy7 <souleymanesyservices@gmail.com>
2025-04-21 20:44:04 +0000

    feat: add the header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,11 @@
 import * as React from "react";
-import Marquee from "@/components/layouts/Marquee";
 
-const marqueeText = [
-  "html",
-  "css",
-  "javascript",
-  "React",
-  "Vue",
-  "Svelte",
-  "Angular",
-  "Typescript",
-  "Astro",
-  "NextJs",
-  "NuxtJs",
-  "TailwindCss",
-  "Bootstrap",
-  "MaterialUi",
-  "ChakraUi",
-  "AntDesign",
-  "Figma",
-  "AdobeXd",
-  "AdobeIllustrator",
-  "AdobePhotoshop",
-  "AdobeAfterEffects",
-  "AdobePremierePro",
-  "AdobeInDesign",
-  "AdobeLightroom",
-  "AdobeXD",
-  "AdobeFresco",
-  "AdobeDimension",
-  "AdobeAnimate",
-  "AdobeAudition",
-  "AdobeCharacterAnimator",
-];
+import Header from "@/components/layouts/Header";
 
 export default function Home() {
   return (
-    <div className="space-y-3 my-4">
-      <Marquee marqueeText={marqueeText} />
-      <Marquee marqueeText={marqueeText} title="News:" />
-    </div>
+    <React.Fragment>
+      <Header />
+    </React.Fragment>
   );
 }
