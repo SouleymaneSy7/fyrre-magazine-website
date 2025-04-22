32085e33dfef917f28b161d238ebfea0c66b8378
souleymanesy7 <souleymanesyservices@gmail.com>
2025-04-22 14:38:21 +0000

    fix: fix the naming errors

--- src/components/common/Form.tsx
+++ src/components/common/Form.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 import { FormPropsType } from "@/types";
 
-const Forms: React.FC<FormPropsType> = ({ children, ...delegatedProps }) => {
+const Form: React.FC<FormPropsType> = ({ children, ...delegatedProps }) => {
   return <form {...delegatedProps}>{children}</form>;
 };
 
-export default Forms;
+export default Form;
