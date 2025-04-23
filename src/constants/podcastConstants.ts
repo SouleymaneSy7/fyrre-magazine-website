752348ab9d54c9a1ff80739e0b5d250aeb08cc88
souleymanesy7 <souleymanesyservices@gmail.com>
2025-04-23 20:14:43 +0000

    feat: add the podcast list constants

---
+++ src/constants/podcastConstants.ts
@@ -0,0 +1,49 @@
+const podcastList = [
+  {
+    id: crypto.randomUUID(),
+    title: "The Problem of today’s cultural development",
+    image: "/public/assets/images/podcast-cover-5.webp",
+    link: "/podcast/the-problem-of-todays-cultural-development",
+    episode: "05",
+    date: "January 21, 2022",
+    duration: "45 Min",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "The hidden messages of Jack Nielson",
+    image: "/public/assets/images/podcast-cover-4.webp",
+    link: "/podcast/the-hidden-messages-of-jack-nielson",
+    episode: "04",
+    date: "January 19, 2022",
+    duration: "1 h 4 Min",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Behind the scenes of the street art culture",
+    image: "/public/assets/images/podcast-cover-3.webp",
+    link: "/podcast/behind-the-scenes-of-the-street-art-culture",
+    episode: "03",
+    date: "January 8, 2022",
+    duration: "56 Min",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "The art of movement",
+    image: "/public/assets/images/podcast-cover-2.webp",
+    link: "/podcast/the-art-of-movement",
+    episode: "02",
+    date: "December 1, 2021",
+    duration: "40 Min",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "10 artists your should definitely know",
+    image: "/public/assets/images/podcast-cover-1.webp",
+    link: "/podcast/10-artists-you-should-definitely-know",
+    episode: "01",
+    date: "October 12, 2021",
+    duration: "1 h 20 Min",
+  },
+];
+
+export { podcastList };
