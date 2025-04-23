271be22e9ad74aa55caa0494361f9e7f331817cb
souleymanesy7 <souleymanesyservices@gmail.com>
2025-04-23 11:27:31 +0000

    feat: add the footer component

---
+++ src/components/layouts/Footer.tsx
@@ -0,0 +1,140 @@
+import * as React from "react";
+import Link from "next/link";
+
+import {
+  socialIcons,
+  FooterHeading,
+  FooterLinkList1,
+  FooterLinkList2,
+  FooterLinkList3,
+} from "@/constants";
+import { LogoIcon } from "@/icons/Icons.component";
+
+import Marquee from "./Marquee";
+import List from "../common/List";
+import Form from "../common/Form";
+import Inputs from "../common/Input";
+import Button from "../common/Button";
+import VisuallyHidden from "../common/VisuallyHidden";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-primary-clr">
+      <Marquee marqueeText={["text-1", "Text-2", "Text-3"]} />
+      <section className="container | text-text-inversed-clr py-x-huge space-y-x-huge lg:space-y-24 lg:pt-32">
+        <div className="flex flex-col gap-x-large lg:flex-row lg:items-center lg:justify-between mb-32">
+          <div className="w-full max-w-[832px]">
+            <h2 className="heading-medium uppercase">{FooterHeading}</h2>
+          </div>
+
+          <Form className="w-full max-w-[480px] flex flex-col gap-3 lg:flex-row lg:gap-4">
+            <Inputs
+              type="email"
+              placeholder="Email"
+              className="form-input | max-w-[360px] bg-default-clr text-text-muted-clr"
+            />
+            <Button
+              variant="default"
+              className="bg-default-clr text-primary-clr"
+            >
+              Sign Up
+            </Button>
+          </Form>
+        </div>
+
+        <div className="flex flex-col gap-x-large lg:flex-row lg:items-start lg:justify-between">
+          <Link href={"/"}>
+            <LogoIcon fill="#fff" />
+            <VisuallyHidden>Fyrre magazine logo</VisuallyHidden>
+          </Link>
+          <hr className="lg:hidden bg-text-muted-clr" />
+
+          <div className="space-y-4 w-full max-w-[300px]">
+            <hr className="hidden lg:block bg-text-muted-clr" />
+            <List
+              items={FooterLinkList1}
+              renderItem={(item) => {
+                return (
+                  <li key={item.id} className="py-2">
+                    <Link href={item.href}>{item.title}</Link>
+                  </li>
+                );
+              }}
+            />
+            <hr className="lg:hidden border-text-muted-clr" />
+          </div>
+
+          <div className="space-y-4 w-full max-w-[300px]">
+            <hr className="hidden lg:block  bg-text-muted-clr" />
+            <List
+              items={FooterLinkList2}
+              renderItem={(item) => {
+                return (
+                  <li key={item.id} className="py-2">
+                    <Link href={item.href}>{item.title}</Link>
+                  </li>
+                );
+              }}
+            />
+            <hr className="lg:hidden bg-text-muted-clr" />
+          </div>
+
+          <div className="space-y-4 w-full max-w-[300px]">
+            <hr className="hidden lg:block bg-text-muted-clr" />
+            <List
+              items={FooterLinkList3}
+              renderItem={(item) => {
+                return (
+                  <li key={item.id} className="py-2">
+                    <Link href={item.href}>{item.title}</Link>
+                  </li>
+                );
+              }}
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center lg:flex-row lg:items-center lg:justify-between">
+          <div className="lg:flex lg:items-center lg:gap-5">
+            <p>
+              © Design by{" "}
+              <Link href={"https://gola.io"} className="text-link">
+                Pawel Gola
+              </Link>
+            </p>
+
+            <p>
+              Code by{" "}
+              <Link
+                href={"https://github.com/souleymanesy7"}
+                className="text-link"
+              >
+                Souleymane Sy
+              </Link>
+            </p>
+          </div>
+
+          <div>
+            <List
+              items={socialIcons}
+              className="flex items-center gap-2"
+              renderItem={(item) => {
+                const Icon = item.Icon;
+
+                return (
+                  <li key={item.id}>
+                    <Link href={item.href}>
+                      <Icon width={24} height={24} fill="#fff" />
+                    </Link>
+                  </li>
+                );
+              }}
+            />
+          </div>
+        </div>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
