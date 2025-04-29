9ed7e324023900e4c608044fa92ee7e157e5a5a6
souleymanesy7 <souleymanesyservices@gmail.com>
2025-04-29 23:22:13 +0000

    feat: add the podcast page & contents

--- src/app/podcast/page.tsx
+++ src/app/podcast/page.tsx
@@ -1,15 +1,33 @@
-import * as React from "react";
-import Headline from "@/components/layouts/Headline";
+import { getArticlesList } from "@/libs/fileHelpers";
 
-const page: React.FC = () => {
+import Headline from "@/components/layouts/Headline";
+import PodcastSummaryCard from "@/components/layouts/PodcastSummaryCard";
+
+const Page = async () => {
+  const podcastList = await getArticlesList("podcast");
+
   return (
     <main id="main-content" className="container">
       <Headline
         imageSrc="/assets/svgs/headline-podcast.svg"
         altText="podcast headline text svg illustration"
       />
+
+      {podcastList.map(({ frontmatter, slug }) => {
+        return (
+          <PodcastSummaryCard
+            key={slug}
+            podcastSlug={slug}
+            podcastDate={frontmatter.date}
+            podcastCover={frontmatter.image}
+            podcastEpisode={frontmatter.episode}
+            podcastDuration={frontmatter.duration}
+            podcastTitle={frontmatter.title}
+          />
+        );
+      })}
     </main>
   );
 };
 
-export default page;
+export default Page;
