f153fe3971f87b9dc5b8c23771bbfb88d6c722eb
souleymanesy7 <souleymanesyservices@gmail.com>
2025-04-29 03:02:55 +0000

    feat: update the authors page

--- src/app/authors/page.tsx
+++ src/app/authors/page.tsx
@@ -1,13 +1,30 @@
-import * as React from "react";
-import Headline from "@/components/layouts/Headline";
+import { getArticlesList } from "@/libs/fileHelpers";
 
-const page: React.FC = () => {
+import Headline from "@/components/layouts/Headline";
+import AuthorSummaryCard from "@/components/layouts/AuthorSummaryCard";
+
+const page = async () => {
+  const avatarList = await getArticlesList("authors");
+
   return (
     <main id="main-content" className="container">
       <Headline
         imageSrc="/assets/svgs/headline-authors.svg"
-        altText="authors headline text svg illustration"
+        altText="authors headline text svg"
       />
+
+      {avatarList.map((author) => {
+        return (
+          <AuthorSummaryCard
+            key={author.slug}
+            authorSlug={author.slug}
+            imageSrc={author.frontmatter.image}
+            authorName={author.frontmatter.name}
+            authorJob={author.frontmatter.job}
+            authorCity={author.frontmatter.city}
+          />
+        );
+      })}
     </main>
   );
 };
