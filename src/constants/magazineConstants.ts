d32392a31e392b299ab54232bf249b492a3d6fa7
souleymanesy7 <souleymanesyservices@gmail.com>
2025-04-30 15:44:00 +0000

    feat: add the magazine constants file

---
+++ src/constants/magazineConstants.ts
@@ -0,0 +1,16 @@
+const magazineCategories = [
+  {
+    id: crypto.randomUUID(),
+    tag: "sculptures",
+  },
+  {
+    id: crypto.randomUUID(),
+    tag: "street art",
+  },
+  {
+    id: crypto.randomUUID(),
+    tag: "art",
+  },
+];
+
+export { magazineCategories };
