ca02872da60c8618982e95abbd815d945e8683dd
souleymanesy7 <souleymanesyservices@gmail.com>
2025-04-30 20:01:36 +0000

    feat: add the magazine post mdx content component

---
+++ src/components/layouts/MagazinePostContents.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import Container from "../common/Container";
+import { MDXRemote } from "next-mdx-remote/rsc";
+
+export type MagazinePostContentsPropsType = {
+  MDXContent: string;
+};
+
+const MagazinePostContents: React.FC<MagazinePostContentsPropsType> = ({
+  MDXContent,
+}) => {
+  return (
+    <Container as={"div"}>
+      <MDXRemote source={MDXContent} />
+    </Container>
+  );
+};
+
+export default MagazinePostContents;
