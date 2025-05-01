d8db0e86aeb7c8a406e22c18a0547bc21c57f182
souleymanesy7 <souleymanesyservices@gmail.com>
2025-05-01 04:29:51 +0000

    feat: update the single nav component with the link with arrow component

--- src/components/layouts/SingleNav.tsx
+++ src/components/layouts/SingleNav.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
-import Link from "next/link";
 
 import Title from "../common/Title";
+import LinkWithArrow from "./LinkWithArrow";
 import Container from "../common/Container";
 
 import { SyngleNavPropsType } from "@/types";
@@ -11,12 +11,14 @@ const SingleNav: React.FC<SyngleNavPropsType> = ({ goBackHref, title }) => {
   return (
     <Container
       as={"section"}
-      className="w-full flex items-center justify-between"
+      className="w-full flex items-center justify-between mt-8"
     >
-      <Link href={`/${goBackHref}`} className="flex items-center gap-2">
-        <ArrowLeftIcon />
-        <span className="uppercase font-semi-bold">Go back</span>
-      </Link>
+      <LinkWithArrow
+        linkHref={`/${goBackHref}`}
+        linkTitle="Go Back"
+        Icon={ArrowLeftIcon}
+        classNames="flex-row-reverse"
+      />
 
       <Title level={"h3"} className="heading-3 uppercase">
         {title}
