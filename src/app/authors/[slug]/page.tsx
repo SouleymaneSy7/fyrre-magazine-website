b03d446c8d213d0c7accf2128f65d9d2e7fec573
souleymanesy7 <souleymanesyservices@gmail.com>
2025-05-01 05:05:33 +0000

    feat(metadata): update the authors profile metadata

--- src/app/authors/[slug]/page.tsx
+++ src/app/authors/[slug]/page.tsx
@@ -16,24 +16,22 @@ export async function generateMetadata({
   params: PagePropsType;
 }): Promise<Metadata> {
   const resolvedParams = await params;
-  const { frontmatter } = await loadArticle(
-    "authors",
-    resolvedParams.slug
-  );
+  const { frontmatter } = await loadArticle("authors", resolvedParams.slug);
 
   return {
-    title: `${frontmatter.name} | Fyrre - Modern Magazine & Digital Content Hub `,
-    description: frontmatter.preview || "No description available",
+    title: frontmatter.name
+      ? `${frontmatter.name} | Fyrre - Modern Magazine & Digital Content Hub.`
+      : "Authors Profile | Fyrre - Modern Magazine & Digital Content Hub.",
+    description:
+      frontmatter.preview ||
+      "Discover Fyrre - Your destination for curated articles, insightful blogs, and engaging podcasts. A clean, modern platform delivering quality content across multiple formats.",
   };
 }
 
 const Page = async (props: { params: PagePropsType }) => {
   const params = await props.params;
 
-  const { frontmatter, content } = await loadArticle(
-    "authors",
-    params.slug
-  );
+  const { frontmatter, content } = await loadArticle("authors", params.slug);
 
   return (
     <main id="main-content" className="container">
