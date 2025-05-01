fb03704a2ca25b353e32b87fc101783acd8597b1
souleymanesy7 <souleymanesyservices@gmail.com>
2025-05-01 15:57:12 +0000

    config: update the next configs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
+  output: "standalone",
   transpilePackages: ["next-mdx-remote"],
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.mdx?$/,
+      use: [
+        {
+          loader: "@mdx-js/loader",
+          options: {
+            providerImportSource: "@mdx-js/react",
+          },
+        },
+      ],
+    });
+    return config;
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
