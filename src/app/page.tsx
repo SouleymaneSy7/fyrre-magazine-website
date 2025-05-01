3c3b3c584b41686bf095e1db446d295746305302
souleymanesy7 <souleymanesyservices@gmail.com>
2025-05-01 01:21:41 +0000

    feat: update the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
-import * as React from "react";
+import Marquee from "@/components/layouts/Marquee";
+import Headline from "@/components/layouts/Headline";
+import { HeaderMarquee } from "@/constants/marqueeConstants";
+import Container from "@/components/common/Container";
+import HomepageHero from "@/components/layouts/HomepageHero";
+import HomepageMagazineList from "@/components/layouts/HomepageMagazineList";
+import HomepageSidebar from "@/components/layouts/HomepageSidebar";
 
 export default function Home() {
   return (
-    <React.Fragment>
-      <div className="w-full h-screen"></div>
-    </React.Fragment>
+    <main id="main-content" className="container">
+      <Headline
+        imageSrc="/assets/svgs/headline-art-life.svg"
+        altText="art life headline text svg illustration"
+      />
+
+      <Marquee title="News:" marqueeText={HeaderMarquee} />
+
+      <HomepageHero />
+
+      <Container as="section">
+        <HomepageMagazineList />
+        <HomepageSidebar />
+      </Container>
+    </main>
   );
 }
