6925d053e40814a1ff18bd52897abd42b8adc1bf
souleymanesy7 <souleymanesyservices@gmail.com>
2025-05-01 03:01:03 +0000

    style(headline): update the healine style

--- src/components/layouts/Headline.tsx
+++ src/components/layouts/Headline.tsx
@@ -8,15 +8,15 @@ const Headline: React.FC<HeadlinePropsType> = ({ imageSrc, altText }) => {
   return (
     <Container
       as={"div"}
-      className="w-full h-[72px] mt-6 md:h-[96px] lg:h-[196px]"
+      className="w-full my-5 h-[clamp(2rem, 1.4718rem + 2.2535vw, 3.5rem)] md:h-[clamp(3.5rem, 2.6197rem + 3.7559vw, 6rem)] lg:h-[clamp(8rem, 6.0634rem + 8.2629vw, 13.5rem)]"
     >
       <Image
-        src={imageSrc}
-        alt={altText}
-        className="w-full h-full"
+        priority
         width={100}
         height={100}
-        priority
+        alt={altText}
+        src={imageSrc}
+        className="w-full h-full"
       />
     </Container>
   );
