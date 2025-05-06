47900d61d00345fc37da045509b61f707a546198
souleymanesy7 <souleymanesyservices@gmail.com>
2025-05-06 16:52:02 +0000

    style: update the homepage style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,13 +26,15 @@ export default function Home() {
 
       <Container
         as="section"
-        className="grid gap-24 lg:grid-cols-[3fr 1fr] lg:grid-rows-1"
+        className="grid gap-24 lg:grid-cols-[3fr 1fr] lg:grid-rows-1 mb-14 md:mb-20 lg:mb-24"
       >
         <HomepageMagazineList />
         <HomepageSidebar />
       </Container>
 
-      <LinkWithArrow linkHref={"/magazine"} linkTitle="All Articles" />
+      <div className="mb-14 md:mb-20 lg:mb-24 ">
+        <LinkWithArrow linkHref={"/magazine"} linkTitle="All Articles" />
+      </div>
 
       <HomepagePodcastList />
 
