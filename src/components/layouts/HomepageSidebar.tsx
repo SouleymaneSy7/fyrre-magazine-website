63c30d35d0ac21962ff4a50f8ab1b0a6c976cc8b
souleymanesy7 <souleymanesyservices@gmail.com>
2025-05-06 15:48:09 +0000

    style: add the homepage aside style

--- src/components/layouts/HomepageSidebar.tsx
+++ src/components/layouts/HomepageSidebar.tsx
@@ -25,7 +25,7 @@ const HomepageSidebar = () => {
             height={625}
             alt="cover image"
             src="/assets/images/cover-image.jpeg"
-            className="w-full h-full aspect-square object-contain"
+            className="w-full h-full aspect-auto"
             sizes="(max-width: 479px) 90vw, (max-width: 991px) 100vw, 18vw"
           />
         </div>
@@ -35,22 +35,29 @@ const HomepageSidebar = () => {
         </Button>
       </div>
 
-      <div>
-        <Title level="h2">Popular Posts</Title>
+      <div className="mt-8 mb-8 md:mt-12 md:mb-12 lg:mt-16 lg:mb-16">
+        <Title level="h2" className="heading-6 uppercase mb-5 md:mb-6 lg:mb-8">
+          Popular Posts
+        </Title>
 
         <div>
           {homepagePopularPostList.map(
             ({ id, title, author, authorLink, listNumber, postLink }) => {
               return (
-                <Card key={id}>
-                  <p>{listNumber}</p>
+                <Card
+                  key={id}
+                  className="flex items-start gap-8 border-b border-b-primary-clr mb-5 pb-5 nth-last-[1]:border-b-0 nth-last-[1]:pb-0 nth-last-[1]:mb-0 md:mb-6 md:pb-6 lg:mb-8 lg:pb-8"
+                >
+                  <p className="heading-4 font-semi-bold">{listNumber}</p>
 
-                  <div>
+                  <div className="flex flex-col gap-4">
                     <Link href={postLink}>
-                      <Title level="h3">{title}</Title>
+                      <Title level="h3" className="heading-4 font-semi-bold">
+                        {title}
+                      </Title>
                     </Link>
 
-                    <div>
+                    <div className="flex items-center gap-2">
                       <strong>Author</strong>
 
                       <Link href={authorLink} className="text-link">
@@ -65,7 +72,7 @@ const HomepageSidebar = () => {
         </div>
       </div>
 
-      <div>
+      <Card className="pt-7 px-8 pb-8 bg-text-muted-clr">
         <Title level="h3">Newsletter</Title>
 
         <Title level="h2">{FooterHeading}</Title>
@@ -74,7 +81,7 @@ const HomepageSidebar = () => {
           <Inputs type="email" placeholder="Email" className="form-input" />
           <Button variant="default">Sign Up</Button>
         </Form>
-      </div>
+      </Card>
     </Container>
   );
 };
