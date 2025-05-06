a7b4d683ad1b14ff194632457547ccb09b3dde8e
souleymanesy7 <souleymanesyservices@gmail.com>
2025-05-06 22:59:57 +0000

    style: add the magazine summary card css style

--- src/components/layouts/MagazineSummaryCard.tsx
+++ src/components/layouts/MagazineSummaryCard.tsx
@@ -6,6 +6,7 @@ import Card from "../common/Card";
 import Title from "../common/Title";
 
 import { MagazineSummaryCardPropsType } from "@/types";
+import Button from "../common/Button";
 
 const MagazineSummaryCard: React.FC<MagazineSummaryCardPropsType> = ({
   magazineAuthor,
@@ -21,35 +22,47 @@ const MagazineSummaryCard: React.FC<MagazineSummaryCardPropsType> = ({
   const authorLink = magazineAuthorLink?.replace(" ", "-").toLowerCase();
 
   return (
-    <Card>
-      <div>
+    <Card className="p-6 lg:p-8 border border-primary-clr -ml-px -mt-px">
+      <div className="flex items-center justify-between mb-5 md:mb-6 lg:mb-8">
         <p>{magazineDate}</p>
-        <p>{magazineTag}</p>
+
+        <Button variant="label" size="label">
+          {magazineTag}
+        </Button>
       </div>
 
-      <Link href={`/magazine/${magazineSlug}`}>
+      <Link
+        href={`/magazine/${magazineSlug}`}
+        className="inline-block w-full h-auto mb-5 md:mb-6 lg:mb-8"
+      >
         <Image
-          width={100}
-          height={100}
+          width={1600}
+          height={1600}
           src={magazineCover}
           alt={`${magazineTitle} - cover image.`}
+          className="w-full h-full aspect-square object-cover"
         />
       </Link>
 
       <div>
-        <Link href={`/magazine/${magazineSlug}`}>
-          <Title level="h2">{magazineTitle}</Title>
+        <Link href={`/magazine/${magazineSlug}`} className="inline-block mb-4">
+          <Title level="h2" className="heading-3">
+            {magazineTitle}
+          </Title>
         </Link>
-        <p>{magazinePreview}</p>
+        <p className="mb-12">{magazinePreview}</p>
 
         <div>
-          <Link href={`/authors/${authorLink}`}>
-            <strong>Text</strong> <span>{magazineAuthor}</span>
-          </Link>
+          <div className="flex items-baseline gap-2">
+            <strong>Text</strong>
+            <Link href={`/authors/${authorLink}`} className="text-link">
+              {magazineAuthor}
+            </Link>
+          </div>
 
-          <p>
+          <div className="flex items-baseline gap-2">
             <strong>Read</strong> <span>{magazineReadTime}</span>
-          </p>
+          </div>
         </div>
       </div>
     </Card>
