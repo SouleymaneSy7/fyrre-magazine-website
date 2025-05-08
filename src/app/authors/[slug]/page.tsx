771e52b65cb1032b4a75e5d84629fe246eea0fcf
souleymanesy7 <souleymanesyservices@gmail.com>
2025-05-08 18:08:28 +0000

    feat: update the authors post page

--- src/app/authors/[slug]/page.tsx
+++ src/app/authors/[slug]/page.tsx
@@ -9,6 +9,7 @@ import SingleNav from "@/components/layouts/SingleNav";
 import AuthorSideBar from "@/components/layouts/AuthorSideBar";
 import AuthorContents from "@/components/layouts/AuthorContents";
 import { PagePropsType } from "@/types";
+import Title from "@/components/common/Title";
 
 export async function generateMetadata({
   params,
@@ -37,15 +38,27 @@ const Page = async (props: { params: PagePropsType }) => {
     <main id="main-content" className="container">
       <SingleNav goBackHref="authors" title="authors" />
 
-      <Container as={"section"}>
+      <Container
+        as={"section"}
+        className="container-medium | flex flex-col gap-12 mb-18 md:gap-16 md:mb-32 lg:gap-24 lg:grid lg:grid-rows-1 lg:grid-cols-3 lg:mb-48"
+      >
         <AuthorSideBar
           imageSrc={frontmatter.image}
+          authorName={frontmatter.name}
           authorJob={frontmatter.job}
           authorCity={frontmatter.city}
           authorSocials={followSocials}
         />
 
         <AuthorContents authorName={frontmatter.name} MDXContent={content} />
+      </Container>
+
+      <Container as={"section"}>
+        <div className="pt-6 md:pt-10 lg:pt-12 border-t border-primary-clr mb-14 md:mb-20 lg:mb-24">
+          <Title level="h2" className="heading-small">
+            Articles by {frontmatter.name}
+          </Title>
+        </div>
       </Container>
     </main>
   );
