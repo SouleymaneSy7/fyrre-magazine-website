4206e3fe14812557193ec732a3563a1d53c6828f
souleymanesy7 <souleymanesyservices@gmail.com>
2025-05-08 18:51:07 +0000

    feat: update the magazine summary card component

--- src/components/layouts/MagazineSummaryCard.tsx
+++ src/components/layouts/MagazineSummaryCard.tsx
@@ -22,47 +22,52 @@ const MagazineSummaryCard: React.FC<MagazineSummaryCardPropsType> = ({
   const authorLink = magazineAuthorLink?.replace(" ", "-").toLowerCase();
 
   return (
-    <Card className="p-6 lg:p-8 border border-primary-clr -ml-px -mt-px">
-      <div className="flex items-center justify-between mb-5 md:mb-6 lg:mb-8">
-        <p>{magazineDate}</p>
-
-        <Button variant="label" size="label">
-          {magazineTag}
-        </Button>
-      </div>
-
-      <Link
-        href={`/magazine/${magazineSlug}`}
-        className="inline-block w-full h-auto mb-5 md:mb-6 lg:mb-8"
-      >
-        <Image
-          width={1600}
-          height={1600}
-          src={magazineCover}
-          alt={`${magazineTitle} - cover image.`}
-          className="w-full h-full aspect-square object-cover"
-        />
-      </Link>
-
+    <Card className="flex flex-col justify-between p-6 md:p-8 lg:p-12 border border-primary-clr -ml-px -mt-px">
       <div>
-        <Link href={`/magazine/${magazineSlug}`} className="inline-block mb-4">
-          <Title level="h2" className="heading-3">
-            {magazineTitle}
-          </Title>
+        <div className="flex items-center justify-between mb-5 md:mb-6 lg:mb-8">
+          <p>{magazineDate}</p>
+
+          <Button variant="label" size="label">
+            {magazineTag}
+          </Button>
+        </div>
+
+        <Link
+          href={`/magazine/${magazineSlug}`}
+          className="inline-block w-full h-auto mb-5 md:mb-6 lg:mb-8"
+        >
+          <Image
+            width={1600}
+            height={1600}
+            src={magazineCover}
+            alt={`${magazineTitle} - cover image.`}
+            className="w-full h-full aspect-square object-cover"
+          />
         </Link>
-        <p className="mb-12">{magazinePreview}</p>
 
         <div>
-          <div className="flex items-baseline gap-2">
-            <strong>Text</strong>
-            <Link href={`/authors/${authorLink}`} className="text-link">
-              {magazineAuthor}
-            </Link>
-          </div>
+          <Link
+            href={`/magazine/${magazineSlug}`}
+            className="inline-block mb-4"
+          >
+            <Title level="h2" className="heading-3">
+              {magazineTitle}
+            </Title>
+          </Link>
+          <p className="mb-12">{magazinePreview}</p>
+        </div>
+      </div>
 
-          <div className="flex items-baseline gap-2">
-            <strong>Read</strong> <span>{magazineReadTime}</span>
-          </div>
+      <div className="flex flex-col gap-2 lg:flex-row lg:items-center lg:gap-6">
+        <div className="flex items-baseline gap-2">
+          <strong>Text</strong>
+          <Link href={`/authors/${authorLink}`} className="text-link">
+            {magazineAuthor}
+          </Link>
+        </div>
+
+        <div className="flex items-baseline gap-2">
+          <strong>Read</strong> <span>{magazineReadTime}</span>
         </div>
       </div>
     </Card>
