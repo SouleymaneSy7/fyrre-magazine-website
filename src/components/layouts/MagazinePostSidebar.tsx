b29ecaeef545ab838357b9bda712f52fd6c0f9d0
souleymanesy7 <souleymanesyservices@gmail.com>
2025-05-08 14:56:07 +0000

    style: update the magazine post sidebar css style

--- src/components/layouts/MagazinePostSidebar.tsx
+++ src/components/layouts/MagazinePostSidebar.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 import List from "../common/List";
 import Title from "../common/Title";
@@ -14,45 +15,63 @@ const MagazinePostSidebar: React.FC<MagazinePostSidebarPropsType> = ({
   magazineReadTime,
   magazineSocials,
 }) => {
-  return (
-    <Container as={"aside"}>
-      <div>
-        <div>
-          <Image
-            src={magazineAuthorImage}
-            alt={`${magazineAuthorName} - author profile image.`}
-            width={100}
-            height={100}
-          />
-        </div>
+  const authorLink = magazineAuthorName?.replace(" ", "-").toLowerCase();
 
-        <Title level="h2">{magazineAuthorName}</Title>
+  return (
+    <Container as={"aside"} className="w-full col-span-1">
+      <div className="w-full flex items-center gap-4 border-b border-primary-clr pb-5 mb-5 md:pb-6 md:mb-6 lg:pb-8 lg:mb-8">
+        <Link
+          href={`/authors/${authorLink}`}
+          className="inline-block w-full h-full max-w-20 max-h-20"
+        >
+          <div className="rounded-full overflow-hidden">
+            <Image
+              src={magazineAuthorImage}
+              alt={`${magazineAuthorName} - author profile image.`}
+              width={620}
+              height={620}
+              className="w-full h-full aspect-square object-contain object-center"
+            />
+          </div>
+        </Link>
+
+        <Link href={`/authors/${authorLink}`}>
+          <Title level="h2" className="heading-3">
+            {magazineAuthorName}
+          </Title>
+        </Link>
       </div>
 
-      <div>
-        <p>
+      <div className="w-full flex flex-col gap-4">
+        <div className="flex items-center justify-between">
           <strong>Date</strong>
           <span>{magazineDate}</span>
-        </p>
+        </div>
 
-        <p>
+        <div className="flex items-center justify-between">
           <strong>Read</strong>
           <span>{magazineReadTime}</span>
-        </p>
+        </div>
 
-        <div>
+        <div className="flex items-center justify-between">
           <p>
             <strong>Share</strong>
           </p>
 
           <List
+            className="flex gap-3 items-center"
             items={magazineSocials}
             renderItem={(item) => {
               const Icon = item.Icon;
 
               return (
                 <li key={item.id}>
-                  <a href={item.href} target="_blank" title={item.name}>
+                  <a
+                    href={item.href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    title={item.name}
+                  >
                     <Icon />
                   </a>
                 </li>
