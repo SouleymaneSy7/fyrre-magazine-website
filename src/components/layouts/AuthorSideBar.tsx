5fa1e73a1db75a2075e47d9ee77b1ff19d90d9e4
souleymanesy7 <souleymanesyservices@gmail.com>
2025-05-08 18:06:56 +0000

    style: update the author sidebar css style

--- src/components/layouts/AuthorSideBar.tsx
+++ src/components/layouts/AuthorSideBar.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { AuthorSideBarPropsType } from "@/types";
 
 import List from "../common/List";
+import Title from "../common/Title";
 import Container from "../common/Container";
 
 const AuthorSideBar: React.FC<AuthorSideBarPropsType> = ({
@@ -11,29 +12,35 @@ const AuthorSideBar: React.FC<AuthorSideBarPropsType> = ({
   authorJob,
   authorCity,
   authorSocials,
+  authorName,
 }) => {
   return (
-    <Container as={"aside"}>
-      <div>
+    <Container as={"aside"} className="col-span-1">
+      <div className="w-full h-auto rounded-full overflow-hidden ">
         <Image
-          width={100}
-          height={100}
+          width={150}
+          height={150}
           src={imageSrc}
-          alt="avatar profile image"
+          alt={`${authorName} - profile image.`}
+          className="w-full h-full"
+          priority
         />
       </div>
 
-      <div>
-        <p>Follow</p>
+      <div className="flex justify-between items-center border-b border-t border-primary-clr py-4 my-5 md:my-6 lg:my-8">
+        <Title level="h3" className="heading-5 uppercase">
+          Follow
+        </Title>
 
         <List
           items={authorSocials}
+          className="flex items-center gap-3"
           renderItem={(item) => {
             const Icon = item.Icon;
 
             return (
               <li key={item.id}>
-                <a href={item.href} target="_blank">
+                <a href={item.href} target="_blank" rel="noopener noreferrer">
                   <Icon />
                 </a>
               </li>
@@ -42,16 +49,16 @@ const AuthorSideBar: React.FC<AuthorSideBarPropsType> = ({
         />
       </div>
 
-      <div>
-        <p>
+      <div className="flex flex-col gap-4">
+        <div className="flex items-center justify-between">
           <strong>Job</strong>
           <span>{authorJob}</span>
-        </p>
+        </div>
 
-        <p>
+        <div className="flex items-center justify-between">
           <strong>City</strong>
           <span>{authorCity}</span>
-        </p>
+        </div>
       </div>
     </Container>
   );
