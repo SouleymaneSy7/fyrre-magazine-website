f8a2a2129c60b82be8b088acf81c06f202521b0b
souleymanesy7 <souleymanesyservices@gmail.com>
2025-05-09 18:25:04 +0000

    style: update the navbar desktop css style

--- src/components/layouts/NavbarDesktop.tsx
+++ src/components/layouts/NavbarDesktop.tsx
@@ -19,14 +19,17 @@ const NavbarDesktop = () => {
         <VisuallyHidden>Fyrre magazine logo</VisuallyHidden>
       </Link>
 
-      <Container as="ul" className="flex items-center gap-4">
+      <Container as="ul" className="flex items-center gap-4 lg:gap-6">
         <List
           items={navbarList}
-          className="flex items-center gap-4"
+          className="flex items-center md:gap-4 lg:gap-6"
           renderItem={(item) => {
             return (
-              <li key={item}>
-                <Link href={`/${item}`} className="capitalize">
+              <li key={item} role="listitem">
+                <Link
+                  href={`/${item}`}
+                  className="capitalize md:text-fs-text-regular lg:text-fs-text-large"
+                >
                   {item}
                 </Link>
               </li>
@@ -38,15 +41,22 @@ const NavbarDesktop = () => {
 
         <List
           items={socialIcons}
-          className="flex items-end gap-2"
+          className="flex items-end gap-2 lg:gap-4"
           renderItem={(item) => {
             const Icon = item.Icon;
 
             return (
-              <li key={item.id}>
-                <Link href={item.href}>
-                  <Icon width={20} height={20} />
-                </Link>
+              <li key={item.id} role="listitem">
+                <a
+                  href={item.href}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label={`follow us on ${item.title}`}
+                  title={`follow us on ${item.title}`}
+                >
+                  <Icon width={22} height={22} />
+                  <VisuallyHidden>follow us on {item.title}</VisuallyHidden>
+                </a>
               </li>
             );
           }}
