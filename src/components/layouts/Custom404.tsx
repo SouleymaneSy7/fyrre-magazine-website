ab5093b01783622a28e50213545a05487f7eceb9
souleymanesy7 <souleymanesyservices@gmail.com>
2025-05-23 10:27:50 +0000

    feat: add the custom 404 component

---
+++ src/components/layouts/Custom404.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import Button from "../common/Button";
+
+const Custom404 = () => {
+  return (
+    <>
+      <main className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <p className="heading-2 text-primary-clr">404</p>
+
+          <h1 className="mt-4 heading-medium uppercase text-balance text-primary-clr">
+            Page not found
+          </h1>
+
+          <p className="mt-6 text-fs-text-large font-medium text-pretty text-text-muted-clr">
+            Sorry, we couldn’t find the page you’re looking for.
+          </p>
+
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <Button variant="default" size="lg" asChild>
+              <Link href={"/"}>Go back home</Link>
+            </Button>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Custom404;
