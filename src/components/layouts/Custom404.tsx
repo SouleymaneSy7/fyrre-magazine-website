cffa666bf2c6e156b9498efd88252de790a7c667
SouleymaneSy7 <souleymanesyservices@gmail.com>
2026-02-24 21:05:41 +0000

    feat(custom404): update the 404 error page

--- src/components/layouts/Custom404.tsx
+++ src/components/layouts/Custom404.tsx
@@ -6,7 +6,12 @@ const Custom404 = () => {
   return (
     <main className="container | grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
       <div className="text-center">
-        <p className="heading-medium ghost-text text-primary-clr">404</p>
+        <p
+          className="heading-medium ghost-text text-primary-clr"
+          aria-hidden="true"
+        >
+          404
+        </p>
 
         <h1 className="mt-4 heading-large uppercase text-balance text-primary-clr">
           Page not found
