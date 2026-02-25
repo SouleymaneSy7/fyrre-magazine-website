99f87fb6d61443438e6ae362b88fc622e30f551a
SouleymaneSy7 <souleymanesyservices@gmail.com>
2026-02-25 23:13:46 +0000

    feat(components/magazines): update the magazines component

--- src/components/layouts/Magazines.tsx
+++ src/components/layouts/Magazines.tsx
@@ -1,32 +1,44 @@
 "use client";
 
-import React from "react";
+import * as React from "react";
 import { useSearchParams } from "next/navigation";
+import { usePagination } from "@/hooks/usePagination";
 
 import Container from "../common/Container";
+import Pagination from "./Pagination";
 import MagazineSummaryCard from "./MagazineSummaryCard";
 import { ArticleDataType, MagazinesPropsType } from "@/types";
+
+const ARTICLES_PER_PAGE = 6;
 
 const Magazines: React.FC<MagazinesPropsType> = ({ magazines }) => {
   const params = useSearchParams();
   const category = params.get("category");
 
   const filteredMagazine = React.useMemo(() => {
-    if (category === "all") {
-      return magazines;
-    } else if (category) {
-      return magazines.filter((item: ArticleDataType) => {
-        return item.frontmatter.tag === category;
-      });
+    if (category === "all") return magazines;
+    if (category) {
+      return magazines.filter(
+        (item: ArticleDataType) => item.frontmatter.tag === category,
+      );
     }
-
     return magazines;
   }, [magazines, category]);
 
+  const {
+    paginatedItems,
+    currentPage,
+    totalPages,
+    goToPrev,
+    goToNext,
+    isFirstPage,
+    isLastPage,
+  } = usePagination(filteredMagazine, ARTICLES_PER_PAGE);
+
   return (
-    <Container as={"section"} className="homepage-podcast-grid  pt-px pl-px">
-      {filteredMagazine.map(({ frontmatter, slug }) => {
-        return (
+    <React.Fragment>
+      <Container as={"section"} className="homepage-podcast-grid pt-px pl-px">
+        {paginatedItems.map(({ frontmatter, slug }) => (
           <MagazineSummaryCard
             key={slug}
             magazineSlug={slug}
@@ -39,9 +51,18 @@ const Magazines: React.FC<MagazinesPropsType> = ({ magazines }) => {
             magazineTag={frontmatter.tag}
             magazineTitle={frontmatter.title}
           />
-        );
-      })}
-    </Container>
+        ))}
+      </Container>
+
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPrev={goToPrev}
+        onNext={goToNext}
+        isFirstPage={isFirstPage}
+        isLastPage={isLastPage}
+      />
+    </React.Fragment>
   );
 };
 
