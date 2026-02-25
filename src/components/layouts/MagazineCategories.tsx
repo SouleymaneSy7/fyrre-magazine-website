e7c09579c1fc7ecb3b0c5f67ff34938d796c878f
SouleymaneSy7 <souleymanesyservices@gmail.com>
2026-02-25 23:13:17 +0000

    feat(components/magazine-categories): update the magazine-categories component

--- src/components/layouts/MagazineCategories.tsx
+++ src/components/layouts/MagazineCategories.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import clsx from "clsx";
-
 import { useRouter, useSearchParams } from "next/navigation";
 
 import List from "../common/List";
@@ -13,36 +12,33 @@ const MagazineCategories = () => {
   const params = useSearchParams();
   const category = params.get("category");
 
-  const handleFilter = (category: string) => {
-    if (category) {
-      router.push("?category=" + category);
-    }
+  const handleFilter = (tag: string) => {
+    const newParams = new URLSearchParams(params.toString());
+    newParams.set("category", tag);
+    newParams.set("page", "1");
+    router.push("?" + newParams.toString());
   };
 
   return (
     <List
       className="flex items-center gap-2 flex-wrap"
       items={magazineCategories}
-      renderItem={(item) => {
-        return (
-          <li key={item.id} role="listitem">
-            <Button
-              size="label"
-              variant="label"
-              className={clsx(
-                category === item.tag
-                  ? "bg-primary-clr text-text-inversed-clr"
-                  : "",
-              )}
-              onClick={() => {
-                handleFilter(item.tag);
-              }}
-            >
-              {item.tag}
-            </Button>
-          </li>
-        );
-      }}
+      renderItem={(item) => (
+        <li key={item.id} role="listitem">
+          <Button
+            size="label"
+            variant="label"
+            className={clsx(
+              category === item.tag
+                ? "bg-primary-clr text-text-inversed-clr"
+                : "",
+            )}
+            onClick={() => handleFilter(item.tag)}
+          >
+            {item.tag}
+          </Button>
+        </li>
+      )}
     />
   );
 };
