450c6c5bb6ac98901c134a93bf00f4b975c7f304
SouleymaneSy7 <souleymanesyservices@gmail.com>
2026-03-02 18:17:27 +0000

    feat: update the homepage-sidebar component

--- src/components/layouts/HomepageSidebar.tsx
+++ src/components/layouts/HomepageSidebar.tsx
@@ -1,15 +1,12 @@
 import Link from "next/link";
-import Image from "next/image";
 
 import Card from "../common/Card";
-import Form from "../common/Form";
 import Title from "../common/Title";
-import Inputs from "../common/Input";
-import Button from "../common/Button";
 import Container from "../common/Container";
+import NewsletterForm from "./NewsletterForm";
+import PrintEditionCard from "./PrintEditionCard";
 
 import { FooterHeading, homepagePopularPostList } from "@/constants";
-import NewsletterForm from "./NewsletterForm";
 
 const HomepageSidebar = () => {
   return (
@@ -17,27 +14,7 @@ const HomepageSidebar = () => {
       as={"aside"}
       className="col-span-2 md:hidden lg:block lg:col-span-1"
     >
-      <div>
-        <div className="mb-5 lg:mb-6">
-          <p className="heading-6 uppercase font-semi-bold">PrintMagazine</p>
-          <h3 className="heading-2">03 / 2022</h3>
-        </div>
-
-        <div className="w-full h-full mb-4">
-          <Image
-            width={500}
-            height={625}
-            alt="cover image"
-            src="/assets/images/cover-image.jpeg"
-            className="w-full h-full aspect-auto"
-            sizes="(max-width: 479px) 90vw, (max-width: 991px) 100vw, 18vw"
-          />
-        </div>
-
-        <Button variant="default" size="default" className="w-full">
-          Buy Now
-        </Button>
-      </div>
+      <PrintEditionCard />
 
       <div className="mt-8 mb-8 md:mt-12 md:mb-12 lg:mt-16 lg:mb-16">
         <Title level="h2" className="heading-6 uppercase mb-5 md:mb-6 lg:mb-8">
@@ -84,11 +61,6 @@ const HomepageSidebar = () => {
         <Title level="h2" className="heading-3 mb-4">
           {FooterHeading}
         </Title>
-
-        {/* <Form className="flex flex-col gap-2"> */}
-        {/* <Inputs type="email" placeholder="Email" className="form-input | border-none bg-default-clr" /> */}
-        {/* <Button variant="default">Sign Up</Button> */}
-        {/* </Form> */}
 
         <NewsletterForm variant="sidebar" />
       </Card>
